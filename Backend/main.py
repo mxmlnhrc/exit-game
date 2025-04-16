1a4ed06f3619dc006f9321e0d912f60a01af5c48
mxmlnhrc <53120138+mxmlnhrc@users.noreply.github.com>
2025-04-16 11:32:07 +0200

    Added simple password check
    Added a POST Route for the backend to check if a password is correct.

--- Backend/main.py
+++ Backend/main.py
@@ -1,7 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query, Form, HTTPException
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
+passwordCheck = "dasisteintest"
+
 @app.get("/")
-def read_root():
-    return {"message": "Willkommen bei FastAPI!"}
+def read_root(q: str = Query(default="")):
+    if q == passwordCheck:
+        return {"message": "Willkommen bei FastAPI!", "query": q}
+    else:
+        return {"message": "Sorry aber das ist wohl nicht die antwort"}
+
+@app.post("/check-pw")
+def check_pw(password: str = Form(...)):
+    if password == passwordCheck:
+        return JSONResponse(content={"message": "Passwort ist richtig"}, status_code=201)
+    else:
+        return HTTPException(status_code=400, detail="Password is incorrect!")
