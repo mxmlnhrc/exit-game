550d15aae52ffa5036b098340d07829a099b460c
mxmlnhrc <53120138+mxmlnhrc@users.noreply.github.com>
2025-05-09 11:46:47 +0200

    Added Countdown
    Countdown einbinden und erstes Styling

--- MainGame/resources/js/countdown.js
+++ MainGame/resources/js/countdown.js
@@ -1,12 +1,16 @@
 let countdownTime = 20 * 60; // 20 Minuten in Sekunden
 
 function startCountdown() {
+    if(sessionStorage.getItem('countdownTime')){
+        countdownTime = parseInt(sessionStorage.getItem('countdownTime'));
+    }
     const countdownElement = document.getElementById('countdown');
     const interval = setInterval(() => {
         const minutes = Math.floor(countdownTime / 60);
         const seconds = countdownTime % 60;
         countdownElement.textContent = `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
         countdownTime--;
+        sessionStorage.setItem('countdownTime', countdownTime);
 
         if (countdownTime < 0) {
             clearInterval(interval);
